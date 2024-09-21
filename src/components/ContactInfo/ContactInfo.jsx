a579c3e5905ca8ab8caefdd92aae8a4f05a06679
Abdulwasiee <wasungncom@gmail.com>
2024-09-21 11:44:06 +0300

    Implement MaterialList and ContactInfo components with CRUD functionality, utilizing context for user roles and IDs. Enhanced styling for improved user experience and presentation.

---
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -0,0 +1,132 @@
+import React, { useContext, useState } from "react";
+import {
+  FaEdit,
+  FaTrashAlt,
+  FaPhone,
+  FaCalendar,
+  FaEnvelope,
+} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../../utility/Axios";
+import styles from "./ContactInfo.module.css";
+import { AuthContext } from "../Auth/Auth";
+
+const ContactInfo = ({ contactData }) => {
+  const { userInfo } = useContext(AuthContext);
+  const { user_id: currentUserId, role_id: roleId } = userInfo;
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [userToDelete, setUserToDelete] = useState(null);
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    const token = localStorage.getItem("authToken");
+
+    try {
+      const response = await axiosInstance.delete(
+        `/api/deleteContact/${userToDelete}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        alert("Contact deleted successfully");
+        // Refresh the page
+        window.location.reload();
+      } else {
+        alert("Error deleting contact");
+      }
+    } catch (error) {
+      console.error("Error deleting contact", error);
+      alert("Network error or failed request");
+    }
+
+    setIsDialogOpen(false);
+  };
+
+  const handleEdit = (userId) => {
+    navigate(`/editContact/${userId}`);
+  };
+
+  const openDialog = (userId) => {
+    setUserToDelete(userId);
+    setIsDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  if (!contactData || contactData.length === 0) {
+    return <p>No contact information available.</p>;
+  }
+
+  const { user_id, name, office_room, phone_number, availability, email } =
+    contactData[0];
+
+  return (
+    <div className={styles.contactInfoContainer}>
+      <h2 className={styles.heading}>Contact Information</h2>
+      <div className={styles.contactCard}>
+        <p>
+          <strong>Name:</strong> {name}
+        </p>
+        <p>
+          <strong>Office Room:</strong> {office_room}
+        </p>
+        <p>
+          <strong>
+            <FaPhone className={styles.icon} /> Phone Number:
+          </strong>{" "}
+          {phone_number}
+        </p>
+        <p>
+          <strong>
+            <FaCalendar className={styles.icon} /> Availability:
+          </strong>{" "}
+          {availability}
+        </p>
+        {email && (
+          <p>
+            <strong>
+              <FaEnvelope className={styles.icon} /> Email:
+            </strong>{" "}
+            {email}
+          </p>
+        )}
+      </div>
+      {(roleId === 3 || roleId === 4) && currentUserId === user_id && (
+        <div className={styles.actionIcons}>
+          <FaEdit
+            className={styles.editIcon}
+            onClick={() => handleEdit(user_id)}
+          />
+          <FaTrashAlt
+            className={styles.deleteIcon}
+            onClick={() => openDialog(user_id)}
+          />
+        </div>
+      )}
+
+      {isDialogOpen && (
+        <div className={styles.overlay}>
+          <div className={styles.dialog}>
+            <h3>Are you sure you want to delete this contact?</h3>
+            <div className={styles.actions}>
+              <button className={styles.confirm} onClick={handleDelete}>
+                Yes
+              </button>
+              <button className={styles.cancel} onClick={closeDialog}>
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ContactInfo;
---
+++ src/components/MaterialList/MaterialList.jsx
@@ -0,0 +1,118 @@
+import React, { useContext, useState } from "react";
+import { FaEdit, FaTrashAlt, FaBook } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../../utility/Axios";
+import * as timeago from "timeago.js";
+import styles from "./MaterialList.module.css";
+import { AuthContext } from "../Auth/Auth";
+
+const MaterialList = ({ materials }) => {
+  const { userInfo } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [materialToDelete, setMaterialToDelete] = useState(null);
+  const roleId = userInfo.role_id;
+
+  const openModal = (material) => {
+    setIsModalOpen(true);
+    setMaterialToDelete(material);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setMaterialToDelete(null);
+  };
+
+  const handleDelete = async () => {
+    const { material_id, title } = materialToDelete;
+    const token = localStorage.getItem("authToken");
+
+    try {
+      const response = await axiosInstance.delete(`/api/deleteFile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: {
+          materialId: material_id,
+          fileKey: title,
+        },
+      });
+
+      if (response.status === 200) {
+        closeModal();
+        window.location.reload();
+      } else {
+        alert("Error deleting material");
+      }
+    } catch (error) {
+      alert("Network error or failed request");
+    }
+  };
+
+  const handleEdit = (materialId) => {
+    navigate(`/editMaterial/${materialId}`);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Materials</h2>
+      <ul className={styles.list}>
+        {materials.length > 0 ? (
+          materials.map((material) => (
+            <li key={material.material_id} className={styles.listItem}>
+              <div className={styles.materialItem}>
+                <FaBook className={styles.bookIcon} />
+                <a
+                  href={material.file_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.link}
+                >
+                  {material.title}
+                </a>
+                <div className={styles.description}>
+                  <p>Uploaded by: {material.uploaded_by}</p>
+                  <p>Created: {timeago.format(material.created_at)}</p>
+                  <p>Last Updated: {timeago.format(material.updated_at)}</p>
+                </div>
+
+                {(roleId === 1 || roleId === 3 || roleId === 5) && (
+                  <div className={styles.actionIcons}>
+                    <FaEdit
+                      className={styles.editIcon}
+                      onClick={() => handleEdit(material.material_id)}
+                    />
+                    <FaTrashAlt
+                      className={styles.deleteIcon}
+                      onClick={() => openModal(material)}
+                    />
+                  </div>
+                )}
+              </div>
+            </li>
+          ))
+        ) : (
+          <p>No materials available for this course.</p>
+        )}
+      </ul>
+
+      {isModalOpen && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalContent}>
+            <p>Are you sure you want to delete this material?</p>
+            <div className={styles.modalActions}>
+              <button onClick={handleDelete} className={styles.confirmButton}>
+                Confirm
+              </button>
+              <button onClick={closeModal} className={styles.cancelButton}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MaterialList;
