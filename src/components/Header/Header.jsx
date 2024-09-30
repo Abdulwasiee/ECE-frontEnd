1333278b45c77b9954a6b88e4175dfb16eb797d9
Abdulwasiee <wasungncom@gmail.com>
2024-09-30 13:32:19 +0300

    header updated to make responsive for different screen using bar icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-import { useLocation, Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import {
   AiOutlineUser,
   AiOutlineSetting,
   AiOutlineLogout,
-  AiOutlineHome, // Import the Home icon
-} from "react-icons/ai"; // Icons for profile, settings, logout, and home
+  AiOutlineHome,
+} from "react-icons/ai";
 import { AuthContext } from "../Auth/Auth";
 import styles from "./Header.module.css";
 
@@ -13,6 +13,8 @@ const Header = () => {
   const { isAuthenticated, logout, userInfo } = useContext(AuthContext);
   const roleId = userInfo.role_id;
   const location = useLocation();
+  const navigate = useNavigate(); // Hook to navigate programmatically
+  const [navVisible, setNavVisible] = useState(false); // State to manage nav visibility
 
   const renderMenu = () => {
     if (!roleId || location.pathname === "/home") return null;
@@ -51,7 +53,11 @@ const Header = () => {
       <ul className={styles.menuList}>
         {menuItems[roleId]?.map((item) => (
           <li key={item.to} className={styles.navItem}>
-            <Link to={item.to} className={styles.navLink}>
+            <Link
+              to={item.to}
+              className={styles.navLink}
+              onClick={() => setNavVisible(false)} // Hide nav on link click
+            >
               {item.text}
             </Link>
           </li>
@@ -60,68 +66,74 @@ const Header = () => {
     );
   };
 
+  const toggleNav = () => {
+    setNavVisible(!navVisible); // Toggle visibility
+  };
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.logoContainer}>
         <h1 className={styles.logoText}>ECE Department</h1>
       </div>
-      <nav className={styles.navContainer}>
-        <ul className={styles.navList}>
-          {isAuthenticated ? (
-            <li onClick={logout} className={styles.navItem}>
-              <AiOutlineLogout className={styles.icon} /> {/* Logout Icon */}
-            </li>
-          ) : (
+      {!isAuthenticated && (
+        <div>
+          <ul>
             <li className={styles.navItem}>
-              <a href="/" className={styles.navLink}>
+              <Link to="/" className={styles.navLink}>
                 Login
-              </a>
-            </li>
-          )}
-        </ul>
-      </nav>
-
-      {/* Render conditional menu based on role and route */}
-      {isAuthenticated && location.pathname !== "/home" && renderMenu()}
-
-      {/* Profile, Settings, Home, and Logout Icons */}
-      {isAuthenticated && (
-        <div className={styles.lowerHeader}>
-          <ul className={styles.profileMenu}>
-            <li className={styles.navItem}>
-              <Link to="/home" className={styles.navLink}>
-                {" "}
-                {/* Home Icon */}
-                <AiOutlineHome
-                  className={`${styles.icon} ${styles.homeIcon}`}
-                />
               </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link
-                to={`/profile`}
-                className={styles.navLink}
-              >
-                <AiOutlineUser
-                  className={`${styles.icon} ${styles.profileIcon}`}
-                />
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link to="/settings" className={styles.navLink}>
-                <AiOutlineSetting
-                  className={`${styles.icon} ${styles.settingIcon}`}
-                />
-              </Link>
-            </li>
-            <li className={styles.navItem} onClick={logout}>
-              <AiOutlineLogout
-                className={`${styles.icon} ${styles.logoutIcon}`}
-              />
             </li>
           </ul>
         </div>
       )}
+      <div>
+        <div className={styles.toggleMenu} onClick={toggleNav}>
+          {navVisible ? "❌" : "☰"} {/* Toggle icon */}
+        </div>
+        <div
+          className={`${styles.navContainer} ${
+            navVisible ? styles.active : ""
+          }`}
+        >
+          {/* Render conditional menu based on role and route */}
+          {isAuthenticated && location.pathname !== "/home" && renderMenu()}
+          {/* Profile, Settings, Home, and Logout Icons */}
+          <div className={styles.icons}>
+            {isAuthenticated && (
+              <div className={styles.lowerHeader}>
+                <ul className={styles.profileMenu}>
+                  <li className={styles.navItem}>
+                    <Link to="/home" className={styles.navLink}>
+                      <AiOutlineHome
+                        className={`${styles.icon} ${styles.homeIcon}`}
+                      />
+                    </Link>
+                  </li>
+                  <li className={styles.navItem}>
+                    <Link to={`/profile`} className={styles.navLink}>
+                      <AiOutlineUser
+                        className={`${styles.icon} ${styles.profileIcon}`}
+                      />
+                    </Link>
+                  </li>
+                  <li className={styles.navItem}>
+                    <Link to="/settings" className={styles.navLink}>
+                      <AiOutlineSetting
+                        className={`${styles.icon} ${styles.settingIcon}`}
+                      />
+                    </Link>
+                  </li>
+                  <li className={styles.navItem} onClick={logout}>
+                    <AiOutlineLogout
+                      className={`${styles.icon} ${styles.logoutIcon}`}
+                    />
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
